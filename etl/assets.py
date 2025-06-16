ff18f91db79a0e1159a3ea31203cea6837e3e88d
Masaori Hirono <masaori@MasaorinoMBP.lan>
2025-06-16 21:19:23 +0900

    qint clientを最新に

--- etl/assets.py
+++ etl/assets.py
@@ -4,21 +4,17 @@ from dagster import asset, AssetExecutionContext # type: ignore
 from dagster_qint import QintClient, UrlListConfig
 
 @asset(
-    deps=[],
     kinds={"s3", "bronze", "python"},
 )
-def example_s3_bronze_asset(
+def ir_document_url_list(
     context: AssetExecutionContext,
     qint_client: QintClient,
+    config: UrlListConfig,
 ):
     """Example asset that reads from S3 and returns a list of dictionaries."""
 
     return qint_client.extract(
         context=context,
         source_type="url_list",
-        config=UrlListConfig(
-            url_list_id="example_s3_bronze_asset",
-            user_id="aaa",
-            urls=["https://example.com/data1.json", "https://example.com/data2.json"],
-        ),
+        config=config,
     )
