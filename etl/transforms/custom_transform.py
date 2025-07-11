44c5829ec77b0687b466bc450a6dca6c0bd6667e
Masaori Hirono <masaori.hirono@gmail.com>
2025-07-11 14:22:19 +0900

    update qbiz

--- etl/transforms/custom_transform.py
+++ etl/transforms/custom_transform.py
@@ -1,2 +1,15 @@
-def main(iceberg_data_query_executor):
-    print(f"This is a placeholder for the custom transform workflow. {iceberg_data_query_executor}")
+import etl_qbiz
+
+def main(data_query_executor: etl_qbiz.DataQueryExecutor):
+    err = data_query_executor.execute_create_table_query(create_table_query=etl_qbiz.DataQueryCreateTable(
+        table_name="hoge",
+        attributes=[
+            etl_qbiz.Column(name="id", type="STRING", nullable=False, comment="ID"),
+            etl_qbiz.Column(name="name", type="STRING", nullable=True, comment="Name"),
+        ],
+        partitions=["id"]
+    ))
+    if err is not None:
+        print(f"Error creating table: {err}")
+    else:
+        print("Table created successfully.")
